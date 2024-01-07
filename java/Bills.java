447705f552a957ee03ffbf722228c7e7db1b134a
Tabitha Anderson <tabitha.anderson@csuglobal.edu>
2024-01-07 13:58:01 -0800

    Final update of project

---
+++ java/Bills.java
@@ -0,0 +1,30 @@
+package averageANDdiscount.java;
+
+
+public class Bills {
+	 
+	
+	public double[] userBills = new double[4];
+	
+	
+	double averageWeek;
+	double averageMonth;
+	
+	public double calculateWeek() {
+		
+		averageWeek = (userBills[0] + userBills[1] +  userBills[2] +  userBills[3]) / 4;
+		
+		return averageWeek;
+	 }
+	
+	public double calculateMonth() {
+
+		averageMonth = (userBills[0] + userBills[1] +  userBills[2] +  userBills[3]);
+		
+		return averageMonth;
+	}
+
+	
+	
+
+}
