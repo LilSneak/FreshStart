95d01e7efa5d1ceef2d858fa1b79e57945f90ff5
Tabitha Anderson <tabitha.anderson@csuglobal.edu>
2024-02-08 19:55:25 -0800

    updated Methods to be more efficient

--- manageCarInfo/mainStart.java
+++ manageCarInfo/mainStart.java
@@ -9,21 +9,24 @@ public class mainStart {
 		
 		autoMobiles autoMobile1 = new autoMobiles();
 		
-		String oldMake;
 		String newModel;
 		String newMake;
 		String newColor;
 		String newYear;
 		String isDone;
+		String updateWhat;
+		int carNum;
+		String updateInfo;
+		
 		do {
-			System.out.println("What would you like to do? Enter one of the following options: Add, Remove, Display, and Update (case-sensitive). Or enter Complete to stop program.");
+			System.out.println("What would you like to do? Enter one of the option numbers: \n 1. Add \n 2. Remove \n 3. Inventory \n 4. Display \n 5. Update \n Or enter \"Complete\" to stop program.");
 			String optionSelect = scnr.next();
 			if (optionSelect.equalsIgnoreCase("Complete")) {
 				break;
 			}
 			else {
 				switch(optionSelect) {
-					case("Add"):
+					case("1"):
 						do {
 							System.out.println("Enter car information in the following order: make, model, color, and year. Only separated by a space.");
 							newMake = scnr.next();
@@ -33,7 +36,7 @@ public class mainStart {
 				
 							autoMobile1.addVehicle(newMake, newModel, newColor, newYear);
 					
-							System.out.println("If you have no more vehicles to add type DONE. Otherwise click any key to continue with prompts.");
+							System.out.println("If you have no more vehicles to add type \"done\". Otherwise click any key to continue with prompts.");
 							isDone = scnr.next();
 							if (isDone.equalsIgnoreCase("done")) {
 								break;
@@ -42,30 +45,49 @@ public class mainStart {
 			
 						System.out.println("Adding Complete.");
 					break;
-					case("Display"):
+					case("2"):
 						if(autoMobile1.autoMake.size()>0) {
-							System.out.println("Enter what make of car you want to see information for.");
-							newMake = scnr.next();
-					
-							autoMobile1.displayVehicle(newMake);
+							System.out.println("Enter car list number you want to remove");
+							carNum = scnr.nextInt();
+							
+							autoMobile1.removeVehicle(carNum);
 						}
 						else {
-							System.out.println("No cars to display");
+							System.out.println("No cars to remove.");
 						}
 					break;
-					case("Update"):
+					case("3"):
+						if(autoMobile1.autoMake.size()>0) {
+							
+							autoMobile1.getInventory();
+						}
+						else {
+							System.out.println("No cars in inventory");
+						}
+					break;
+					case("4"):
+						if(autoMobile1.autoMake.size()>0) {
+							System.out.println("Enter car list number you only want displayed.");
+							carNum = scnr.nextInt();
+					
+							autoMobile1.displayVehicle(carNum);
+						}
+						else {
+							System.out.println("No car to display");
+						}
+					break;
+					case("5"):
 						do {
 							if(autoMobile1.autoMake.size()>0) {
-								System.out.println("Enter car make you want to update");
-								oldMake = scnr.next();
+								System.out.println("Enter car list number you want to update.");
+								carNum = scnr.nextInt();
 								
-								System.out.println("Enter new car make, model, color and year of car you want to update.");
-								newMake = scnr.next();
-								newModel = scnr.next();
-								newColor = scnr.next();
-								newYear = scnr.next();
+								System.out.println("Enter what you want to update. Options are: \"Make\", \"Model\", \"Color\", and \"Year\" (case-sensitive)");
+								updateWhat = scnr.next();
+								System.out.println("Enter what your new input");
+								updateInfo = scnr.next();
 								
-								autoMobile1.updateVehicle(oldMake, newMake, newModel, newColor, newYear);
+								autoMobile1.updateVehicle(carNum, updateWhat, updateInfo);
 								
 								System.out.println("If you are done updating vehicles type DONE. Otherwise click any key to continue with prompts.");
 								isDone = scnr.next();
@@ -79,17 +101,7 @@ public class mainStart {
 							}
 						} while(true);
 					break;
-					case("Remove"):
-						if(autoMobile1.autoMake.size()>0) {
-							System.out.println("Enter what make of car you want to remove");
-							newMake = scnr.next();
-							
-							autoMobile1.removeVehicle(newMake);
-						}
-						else {
-							System.out.println("No cars to remove.");
-						}
-					break;
+			
 				}
 			}
 		} while(true);
